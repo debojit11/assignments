6f7824ff37621232205819c9d596b2f0f6a93f4e
debojit11 <debojitchoudhury117@gmail.com>
2024-08-17 17:16:46 +0530

    third commit

---
+++ 05_machine-setup/05_02_yeehaw.py
@@ -0,0 +1,6 @@
+# In this script, write code that displays the follow message:
+#
+# 	I'm a programmer now.
+# 	Yeehaw!
+# 	Coding here I come!
+print("I'm a programmer now.\nYeehaw!\nCoding here I come!")
