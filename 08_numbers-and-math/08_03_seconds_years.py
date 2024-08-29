6989db0414ab67f08aa1b4a77ae2d176bd7c0ad2
debojit11 <debojitchoudhury117@gmail.com>
2024-08-29 17:38:59 +0530

    08_numbers-and-math

---
+++ 08_numbers-and-math/08_03_seconds_years.py
@@ -0,0 +1,12 @@
+# Move the code you previously wrote to calculate
+# how many seconds are in a year into this file.
+# Then execute it as a script and print the output to your console.
+# Calculate the number of seconds in a year
+
+days_in_year = 365
+hours_in_day = 24
+minutes_in_hour = 60
+seconds_in_minute = 60
+seconds_in_year = days_in_year * hours_in_day * minutes_in_hour * seconds_in_minute
+
+print(seconds_in_year)
