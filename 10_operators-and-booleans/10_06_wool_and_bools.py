584c724c7b02b65681b7d77ac2e2837c6354e110
debojit11 <debojitchoudhury117@gmail.com>
2024-08-31 21:45:06 +0530

    10_operators-and-booleans

---
+++ 10_operators-and-booleans/10_06_wool_and_bools.py
@@ -0,0 +1,8 @@
+# Write a code snippet that gives a name to a `sheep`
+# and uses a boolean value to define whether it has `wool` or not.
+
+sheep_name = "Fuffy"
+sheep_has_wool = True
+
+print(sheep_name) 
+print(sheep_has_wool)  
