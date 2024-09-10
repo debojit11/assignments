7e734ee336ba51e9f1fef4cba3cbe7a38af27f1f
debojit11 <126899332+debojit11@users.noreply.github.com>
2024-09-10 19:05:43 +0530

    Delete 02_more-datatypes directory

--- 02_more-datatypes/02_01_converter.py
+++
@@ -1,9 +0,0 @@
-# Convert a string to a tuple and print out the result.
-# What do you see?
-# What happens if you try to iterate over your tuple of characters?
-# Do you notice any difference to iterating over the string?
-
-string = "codingnomads"
-print(tuple(string))
-for i in tuple(string):
-    print(i)
